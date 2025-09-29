f970b0b565ddbebf82a72e7c0ab7dd202c50c3b4
Edclydson <edclydson.sousa@gmail.com>
2025-09-29 20:46:58 -0300

    :recycle: adicionando loop para verificar a existencia de arquivo na source

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import time
 from cleaner.cleaner import FileEraser
 from reader.reader import File
 from sender.sender import TrendSender
+import os
 
 if __name__ == "__main__":
-    data = File.read_csv()
-    TrendSender.send(data)
-    FileEraser.erase()
+    while True:
+        if os.listdir("source") == []:
+            continue
+        time.sleep(5)
+        data = File.read_csv()
+        TrendSender.send(data)
+        FileEraser.erase()
+
 
